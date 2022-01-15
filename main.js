081d65af84ac848f418b69e01bf29f147ee14143
Sardo <sardo.ip@sardo.work>
2022-01-15 15:50:46 +0800

    Remove config.json as not used

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
 import fs from 'fs';
 import Weather from './lib/weather.js';
 
-const configFile = fs.readFileSync('config.json');
-const config = JSON.parse(configFile);
-
-const baseUrl = config.apiUrl;
-const format = config.format;
-
 const HKWeather = {
+  baseUrl: 'https://www.hko.gov.hk/wxinfo/dailywx/yeswx/DYN_DAT_MINDS_RYES',
+  format: 'json',
   async getAllTemp(date){
     //All of the date which input by user should be formated to call API.
     const formatedDate = date.replace(/\//g, '');
-    const weather = await Weather.fetchWeather(baseUrl, format, formatedDate);
+    const weather = await Weather.fetchWeather(this.baseUrl, this.format, formatedDate);
     return weather;
   },
   async getAreaTemp(date, area){
     const formatedDate = date.replace(/\//g, '');
-    const weather = await Weather.fetchWeather(baseUrl, format, formatedDate);
+    const weather = await Weather.fetchWeather(this.baseUrl, this.format, formatedDate);
     return weather;
   },
   async getRainfall(date){
     const formatedDate = date.replace(/\//g, '');
-    const weather = await Weather.fetchWeather(baseUrl, format, formatedDate);
+    const weather = await Weather.fetchWeather(this.baseUrl, this.format, formatedDate);
     const raining = weather['DYN_DAT_MINDS_RYES']['HKOReadingsRainfall']['Val_Eng'];
     return raining;
   },
